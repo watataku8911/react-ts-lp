4649e640e5a9f4775b8b3c43ccd77c515496f491
watataku8911 <watato.takuya@gmail.com>
2021-09-03 18:25:15 +0900

    opning animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,54 @@
-import React, {ComponentProps} from 'react';
-import './App.css';
-import 'animate.css/animate.css'
-import data from "./data.json"
+import React, { ComponentProps, useEffect, useState } from "react";
+import "./App.css";
+import "animate.css/animate.css";
+import data from "./data.json";
 
-import MainImage from "./components/MainImage/MainImage"
-import Card from "./components/Card/Card"
-import Form from "./components/Form/Form"
-import Footer from "./components/Footer/Footer"
+import MainImage from "./components/MainImage/MainImage";
+import Card from "./components/Card/Card";
+import Form from "./components/Form/Form";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setTimeout(() => {
+        setIsLoading(true);
+      }, 1000);
+    });
+  }, [setIsLoading]);
 
   return (
     <div className="App">
-      <div>
-        <MainImage />
-        <div className="card">
-          {data.map((item: ComponentProps<typeof Card>, index: number) => {
-            return(
-              <Card title={item.title} description={item.description} imagePath={item.imagePath} key={index} />
-            )
-          })}
+      {!isLoading && (
+        <div className="opning">
+          <p>Now Loading...</p>
         </div>
-        <h2 className="form_h2">Form</h2>
-        <Form />
-        <div className="module--spacing--small"></div>
-        <Footer />
-      </div>
+      )}
+      {isLoading && (
+        <div className="wrapper">
+          <MainImage />
+          <div className="card">
+            {data.map((item: ComponentProps<typeof Card>, index: number) => {
+              return (
+                <Card
+                  title={item.title}
+                  description={item.description}
+                  imagePath={item.imagePath}
+                  key={index}
+                />
+              );
+            })}
+          </div>
+          <h2 className="form_h2">Form</h2>
+          <Form />
+          <div className="module--spacing--small"></div>
+          <Footer />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
