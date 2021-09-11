2550ef6007c224b512f283cd87b3a643177fdd13
Watataku <39507718+watataku8911@users.noreply.github.com>
2021-09-11 17:34:15 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps, useEffect, useState } from "react";
+import React, { ComponentProps } from "react";
 import "./App.css";
 import "animate.css/animate.css";
 import data from "./data.json";
@@ -9,51 +9,27 @@ import Form from "./components/Form/Form";
 import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState<Boolean>(false);
-  const [bar, setBar] = useState<Boolean>(false);
-
-  useEffect(() => {
-    window.addEventListener("load", () => {
-      loadFinish();
-    });
-  }, [setIsLoading, setBar]);
-
-  const loadFinish = () => {
-    setBar(true);
-    setTimeout(() => {
-      setIsLoading(true);
-    }, 1600);
-  };
-
   return (
     <div className="App">
-      {!isLoading && (
-        <div className="opning">
-          {bar && <div className="bar"></div>}
-          <p>Now Loading...</p>
+      <div className="wrapper">
+        <MainImage />
+        <div className="card">
+          {data.map((item: ComponentProps<typeof Card>, index: number) => {
+            return (
+              <Card
+                title={item.title}
+                description={item.description}
+                imagePath={item.imagePath}
+                key={index}
+              />
+            );
+          })}
         </div>
-      )}
-      {isLoading && (
-        <div className="wrapper">
-          <MainImage />
-          <div className="card">
-            {data.map((item: ComponentProps<typeof Card>, index: number) => {
-              return (
-                <Card
-                  title={item.title}
-                  description={item.description}
-                  imagePath={item.imagePath}
-                  key={index}
-                />
-              );
-            })}
-          </div>
-          <h2 className="form_h2">Form</h2>
-          <Form />
-          <div className="module--spacing--small"></div>
-          <Footer />
-        </div>
-      )}
+        <h2 className="form_h2">Form</h2>
+        <Form />
+        <div className="module--spacing--small"></div>
+        <Footer />
+      </div>
     </div>
   );
 };
