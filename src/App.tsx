b72a8a8abe1d859a1739c1197c53e3ccbe4a6eff
Watataku <39507718+watataku8911@users.noreply.github.com>
2021-10-05 15:38:40 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,23 @@ import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <div className="App">
-      <div className="wrapper">
-        <MainImage />
-        <div className="card">
-          {data.map((item: ComponentProps<typeof Card>, index: number) => {
-            return (
-              <Card
-                title={item.title}
-                description={item.description}
-                imagePath={item.imagePath}
-                key={index}
-              />
-            );
-          })}
-        </div>
-        <h2 className="form_h2">Form</h2>
-        <Form />
-        <div className="module--spacing--small"></div>
-        <Footer />
+      <MainImage />
+      <div className="card">
+        {data.map((item: ComponentProps<typeof Card>, index: number) => {
+          return (
+            <Card
+              title={item.title}
+              description={item.description}
+              imagePath={item.imagePath}
+              key={index}
+            />
+          );
+        })}
       </div>
+      <h2 className="form_h2">Form</h2>
+      <Form />
+      <div className="module--spacing--small"></div>
+      <Footer />
     </div>
   );
 };
